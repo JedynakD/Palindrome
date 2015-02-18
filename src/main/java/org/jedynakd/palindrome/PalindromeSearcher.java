8f8a07978e4f595c2681a81d35f9872a83560b5e
JedynakD <djedynak90@gmail.com>
2015-02-18 14:16:37 +0100

    added communication with user

--- src/main/java/org/jedynakd/palindrome/PalindromeSearcher.java
+++ src/main/java/org/jedynakd/palindrome/PalindromeSearcher.java
@@ -9,26 +9,25 @@ public class PalindromeSearcher {
         int digit;
         int number;
         List<Integer> palindromes = new ArrayList<>();
-        Scanner in = new Scanner(System.in);
 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter how many digits.");
         digit = in.nextInt();
+        System.out.println("Enter how many numbers.");
         number = in.nextInt();
 
         String strMaxNumber = "";
         for (int i = 0; i < digit; i++) {
             strMaxNumber = 9 + strMaxNumber;
         }
-
         int maxNumber = Integer.parseInt(strMaxNumber);
         int maxPossiblePalindrome = 1;
-
         for (int j = 0; j < number; j++) {
             maxPossiblePalindrome = maxPossiblePalindrome * maxNumber;
         }
 
         String strNumber;
-        int lowestDivisor = (maxNumber+1)/10;
-        System.out.println(lowestDivisor);
+        int lowestDivisor = (maxNumber + 1) / 10;
         while (maxPossiblePalindrome != lowestDivisor) {
             strNumber = Integer.toString(maxPossiblePalindrome);
             if (strNumber.equals(new StringBuffer(strNumber).reverse().toString())) {
@@ -36,13 +35,13 @@ public class PalindromeSearcher {
             }
             maxPossiblePalindrome--;
         }
+
         int checker = 0;
         maxNumber = Integer.parseInt(strMaxNumber);
-
         while (maxNumber != lowestDivisor) {
             for (Integer intPalindrome : palindromes) {
                 if (intPalindrome % maxNumber == 0 && checker == 0) {
-                    System.out.println(intPalindrome);
+                    System.out.println("Biggest palindrome made from multiplication of " + number + " numbers each consisting " + digit + " digits is " + intPalindrome);
                     checker++;
                 }
             }
